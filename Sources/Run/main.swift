5b180e70117c1f13b699b7e6a2bc01fd99bf00c4
TheInkedEngineer <firas@hey.com>
2022-12-09 12:33:25 +0000

    Update code

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -2,7 +2,7 @@ import MockServer
 import Vapor
 
 let server = MockServer()
-try? server.configure(using: ServerConfiguration())
+try? server.configure(using: ServerConfiguration(networkExchanges: Demo.networkExchanges))
 try? server.start()
 
 defer {
@@ -11,51 +11,9 @@ defer {
   }
 }
 
-
 struct ServerConfiguration: ServerConfigurationProvider {
-  var networkExchanges: [MockServer.NetworkExchange] = [
-    MockServer.NetworkExchange(
-      request: MockServer.Request(method: .GET, path: ["json"]),
-      response: MockServer.Response(
-        kind: .json(Person(name: "Jason"))
-      )
-    ),
-    
-    MockServer.NetworkExchange(
-      request: MockServer.Request(method: .GET, path: ["data"]),
-      response: MockServer.Response(
-        headers: ["Content-Type": "application/json"],
-        kind: .data(try! JSONEncoder().encode(Person(name: "Data")))
-      )
-    ),
-    
-    MockServer.NetworkExchange(
-      request: MockServer.Request(method: .GET, path: ["string"]),
-      response: MockServer.Response(
-        kind: .string("String")
-      )
-    ),
-    
-    MockServer.NetworkExchange(
-      request: MockServer.Request(method: .GET, path: ["file"]),
-      response: MockServer.Response(
-        kind: .fileContent(pathToFile: Bundle.module.path(forResource: "Resources/file", ofType: "json")!)
-      )
-    ),
-    MockServer.NetworkExchange(
-      request: MockServer.Request(method: .GET, path: ["image"]),
-      response: MockServer.Response(
-        kind: .fileContent(pathToFile: Bundle.module.path(forResource: "Resources/vapor", ofType: "jpg")!)
-      )
-    )
-  ]
-  
-  var environment: MockServer.Environment = .development
-  
+  let environment: MockServer.Environment = .development
   let hostname: String = "127.0.0.1"
   let port: Int = 8080
-}
-
-struct Person: Encodable {
-  let name: String
+  let networkExchanges: [MockServer.NetworkExchange]
 }
