c66964dfedb594c44b00c3222020308d71c22691
TheInkedEngineer <firas@hey.com>
2023-01-08 18:57:03 +0100

    Remove unsafe flag

--- Package.swift
+++ Package.swift
@@ -22,12 +22,6 @@ let package = Package(
             name: "EMMockServer",
             dependencies: [
                 .product(name: "Vapor", package: "vapor")
-            ],
-            swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
         .executableTarget(
