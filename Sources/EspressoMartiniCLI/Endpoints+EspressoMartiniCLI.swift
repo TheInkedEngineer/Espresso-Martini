de11cbf877959800c0e039c4ff23735a5f378ffb
TheInkedEngineer <firas@hey.com>
2023-02-12 09:18:35 +0100

    Add code to enhance printing

--- Sources/EspressoMartiniCLI/Endpoints+EspressoMartiniCLI.swift
+++ Sources/EspressoMartiniCLI/Endpoints+EspressoMartiniCLI.swift
@@ -16,14 +16,26 @@ extension EspressoMartiniCLI {
     )
     var baseFolder: String
     
-    @Flag
-    var verbose = false
-    
     func run() throws {
       let baseFolderURL = URL(string: FileManager.default.currentDirectoryPath)!.appending(baseFolder)
       
-      try Helpers.generateEndpoints(from: baseFolderURL, networkExchangesFolder: baseFolder).forEach {
-        $0.prettyPrint(verbose: verbose)
+      let networkExchanges = try Helpers.generateEndpoints(from: baseFolderURL, networkExchangesFolder: baseFolder)
+      let groupedNetworkExchanges = Dictionary(grouping: networkExchanges, by: {$0.request.pathAsString})
+      groupedNetworkExchanges.forEach {
+        print("Endpoint: \($0.key)")
+        $0.value.forEach {
+          print("|-- Method: \($0.request.method)")
+          $0.response
+            .enumerated()
+            .forEach {
+              print("    |-- Response number \($0.offset + 1):")
+              print("        |-- Expected Status Code: \($0.element.status.code)")
+              print("        |-- Expected Headers: \($0.element.headers)")
+              print("        |-- Expected Response kind: \($0.element.kind)")
+              print("        |-- Expected Response Delay: \(($0.element.delay != nil) ? String($0.element.delay!) : "N/A")")
+              print("")
+          }
+        }
       }
     }
   }
